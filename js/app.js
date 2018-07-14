ec26fd7aa73ad3d1cecd56c9b43be62e60e435c1
John Carney <jcarney000@gmail.com>
2018-07-14 16:22:29 -0700

    fix trigger bug

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 'use strict';
-const aframe = require('aframe');
+
+require('aframe');
 require('aframe-physics-system');
 
-aframe.registerComponent('rocktrigger', {
-  init: function () {
-    var trigger = this.el;
-    var rock = document.getElementById('rock');
-
-    trigger.addEventListener('mouseenter', function () {
-      rock.setAttribute('dynamic-body', '');
-    });
+AFRAME.registerComponent(
+  'rock',
+  {
+    init: function () {
+      var trigger = this.el;
+      var rock = document.getElementById('rock');
+      trigger.addEventListener('mouseenter', function () {
+        rock.setAttribute('dynamic-body', '');
+      });
+    }
   }
-});
+);
 
